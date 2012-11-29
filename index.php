2ccc4badfcfaad71beb679b570495e14c0a73dfc
Specter <Specter.hi@gmail.com>
2012-11-29 15:14:24 +0800

    初始化

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/** index.php
+ *
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @author		Konstantin Obenland
+ * @package		The Bootstrap
+ * @since		1.0.0 - 05.02.2012
+ */
+
+get_header(); ?>
+
+get_sidebar();
+get_sidebar(); ?>
+get_footer();
+
+
+/* End of file index.php */
+/* Location: ./wp-content/themes/the-bootstrap/index.php */
