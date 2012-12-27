0fb7785ab13c8cdfb71df38cfdfa5fdd44657f3b
Specter.Hi <Specter.hi@gmail.com>
2012-12-28 04:06:24 +0800

    搜索结果计数及样式

---
+++ partials/content-search.php
@@ -0,0 +1,51 @@
+<?php
+/** content.php
+ *
+ * The default template for displaying content
+ *
+ * @author		Konstantin Obenland
+ * @package		The Bootstrap
+ * @since		1.0.0 - 05.02.2012
+ */
+
+
+tha_entry_before(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php tha_entry_top(); ?>
+
+
+	<header class="searchpostitle">
+	<?php
+				$categories_list = get_the_category_list( _x( ', ', 'used between list items, there is a space after the comma', 'the-bootstrap' ) );
+			printf( '<div class="row"><div class="postin span2">' . __( '%1$s ', 'the-bootstrap' ) . '</div>', $categories_list );
+
+			the_title( '<div class="entry-title span6"><a href="' . get_permalink() .'" title="' . sprintf( esc_attr__( 'Permalink to %s', 'the-bootstrap' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark">', '</a></div>' );
+		
+/*不显示发表时间等信息
+		if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php the_bootstrap_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; 
+不显示发表时间等信息*/  ?>
+
+	</header><!-- .entry-header -->
+
+	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+	<div class="entry-summary clearfix">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php endif; ?>
+
+
+	<footer class="entry-meta">
+	</footer><!-- #entry-meta -->
+
+	
+	<?php tha_entry_bottom(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
+<?php tha_entry_after();
+
+
+/* End of file content.php */
+/* Location: ./wp-content/themes/the-bootstrap/partials/content.php */
--- search.php
+++ search.php
@@ -18,17 +18,18 @@ get_header(); ?>
 		if ( have_posts() ) : ?>
 	
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'the-bootstrap' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title">                Search Results for: <?php /* Search Count */ $allsearch = &new WP_Query("s=$s&showposts=-1"); $key = wp_specialchars($s, 1); $count = $allsearch->post_count; _e(''); _e('<span class="search-terms">'); echo $key; _e('</span>'); _e(' - '); echo $count . ' '; _e('results'); wp_reset_query(); ?></h1>
+
 			</header>
 	
 			<?php
 			while ( have_posts() ) {
 				the_post();
-				get_template_part( '/partials/content', get_post_format() );
+				get_template_part( '/partials/content-search', get_post_format() );
 			}
 			the_bootstrap_content_nav();
 		else :
-			get_template_part( '/partials/content', 'not-found' );
+			get_template_part( '/partials/content-search', 'not-found' );
 		endif; 
 	
 		tha_content_bottom(); ?>
